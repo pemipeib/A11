9102ec06951b16a02cb498ff6e3437ce64432e30
pemipeib <pemipeib@msn.com>
2024-03-22 17:22:52 +0100

    Inicio

---
+++ src/Actividad01/A01.java
@@ -0,0 +1,30 @@
+package Actividad01;
+
+import java.util.*;
+
+public class A01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void menu(){
+        int opt= 0;
+
+        Scanner pedirDatos= new Scanner(System.in);
+
+        String menu= "";
+
+    }
+    public static void crearFichero(){
+
+    }
+
+    public static void verContenido(){
+
+    }
+
+    public static void verInf(){
+
+    }
+}
