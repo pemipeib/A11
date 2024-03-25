9ede4c98e29ff4d7b12d3dae27708b3b19825865
pedro <pedro@ppedro.com>
2024-03-25 23:16:14 +0100

    A04 acabado

--- src/A04.java
+++ src/A04.java
@@ -7,25 +7,25 @@ clase BufferedWriter.
 quiere borrar del fichero y lo elimine. */
 
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class A04 {
-//    Poner el directorio a usar
+    //    Poner el directorio a usar
 //    static String directorio= "c:\\Grado superior\\PRG\\11\\para enviar\\Unidad11\\";
-    static String directorio= "/home/pedro/programas/java/A11";
+    static String directorio = "/home/pedro/programas/java/A11/";
 
     static File file = new File(directorio, "Fichero4.txt");
+
     public static void main(String[] args) {
         insertarAlumnos();
         elminaralumno();
     }
 
-    public static void insertarAlumnos(){
+    public static void insertarAlumnos() {
 
-        String[] alumnos= {
+        String[] alumnos = {
                 "alumno 1",
                 "alumno 2",
                 "alumno 3",
@@ -40,15 +40,15 @@ public class A04 {
         try {
             if (file.createNewFile()) {
                 //        Poner el texto en el fichero
-                try{
-                    FileWriter texto= new FileWriter(file);
-                    BufferedWriter bufferTexto= new BufferedWriter(texto);
-                    for (String alumno : alumnos){
+                try {
+                    FileWriter texto = new FileWriter(file);
+                    BufferedWriter bufferTexto = new BufferedWriter(texto);
+                    for (String alumno : alumnos) {
                         bufferTexto.write(alumno);
                         bufferTexto.newLine();
                     }
                     bufferTexto.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else {
@@ -59,29 +59,29 @@ public class A04 {
         }
     }
 
-    public static void elminaralumno(){
-        ArrayList<String> alumnos= new ArrayList<>();
+    public static void elminaralumno() {
+        ArrayList<String> alumnos = new ArrayList<>();
 
-        Scanner pedirDatos= new Scanner(System.in);
+        Scanner pedirDatos = new Scanner(System.in);
 
-        boolean salir= false;
+        boolean salir = false;
 
-        String alu= "", leido;;
+        String alu = "", leido;
 
         //        Leer contenido del fichero
         if (file.exists()) {
             try {
-                FileReader leer= new FileReader(file);
-                BufferedReader bufferLeer= new BufferedReader(leer);
+                FileReader leer = new FileReader(file);
+                BufferedReader bufferLeer = new BufferedReader(leer);
 
 //                lee las líneas una a una hasta que devuelve null
                 while ((leido = bufferLeer.readLine()) != null) {
                     alumnos.add(leido);
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }else {
+        } else {
             System.out.println("El fichero " + file.getName() + " no existe");
         }
 
@@ -90,42 +90,42 @@ public class A04 {
 
             try {
                 System.out.println("Dime el nombre del alumno a eliminar");
-                for (String alumno : alumnos){
+                for (String alumno : alumnos) {
                     System.out.println(alumno);
                 }
-                alu= pedirDatos.nextLine();
-            }catch (InputMismatchException e){
+                alu = pedirDatos.nextLine();
+            } catch (InputMismatchException e) {
                 System.out.println("Solo números");
                 pedirDatos.nextLine();
             }
 
 //            Recorre el arayList y elimina alumno
             for (int i = 0; i < alumnos.size(); i++) {
-                if (alu.equalsIgnoreCase(alumnos.get(i))){
+                if (alu.equalsIgnoreCase(alumnos.get(i))) {
                     alumnos.remove(alumnos.get(i));
                     System.out.println("El alumno " + alu + " ha sido eliminado del fichero");
-                    salir= true;
+                    salir = true;
 //                    Borra fichero4.txt
-                    if (file.exists()){
+                    if (file.exists()) {
                         file.delete();
                     }
                 }
             }
-        }while (!salir);
+        } while (!salir);
 
 //        Crea otra vez y añado el contenido del arrayList a fichero4.txt
         try {
             if (file.createNewFile()) {
 //          Poner el texto en el fichero
-                try{
-                    FileWriter texto= new FileWriter(file);
-                    BufferedWriter bufferTexto= new BufferedWriter(texto);
-                    for (String alumno : alumnos){
+                try {
+                    FileWriter texto = new FileWriter(file);
+                    BufferedWriter bufferTexto = new BufferedWriter(texto);
+                    for (String alumno : alumnos) {
                         bufferTexto.write(alumno);
                         bufferTexto.newLine();
                     }
                     bufferTexto.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             } else {
--- src/A05.java
+++ src/A05.java
@@ -6,5 +6,53 @@ solicitar al usuario el nombre de los ficheros y la tira de separación entre la
 también se deberá insertar, o directamente ponerlo en el programa.
 */
 
+import java.io.*;
+
 public class A05 {
+    //    Poner el directorio a usar
+    static String directorio = "/home/pedro/Documentos/";
+
+    static File file1 = new File(directorio, "Fichero05a.txt");
+    static File file2 = new File(directorio, "Fichero05b.txt");
+    static File file3 = new File(directorio, "Ficherounion.txt");
+
+    public static void main(String[] args) {
+        unirFichero();
+    }
+
+
+    public static void unirFichero() {
+
+        if (file3.exists()) {
+            file3.delete();
+        }
+
+        try {
+
+            FileReader leer1 = new FileReader(file1);
+            BufferedReader bufferLeer1 = new BufferedReader(leer1);
+
+            FileReader leer2 = new FileReader(file2);
+            BufferedReader bufferLeer2 = new BufferedReader(leer2);
+
+            FileWriter texto = new FileWriter(file3);
+            BufferedWriter bufferTexto = new BufferedWriter(texto);
+
+//                lee las líneas una a una hasta que devuelve null.
+//            Da problemas si no se pone una variable como , en este caso, leido1 o leido2
+            String leido1, leido2;
+            while ((leido1 = bufferLeer1.readLine()) != null && (leido2 = bufferLeer2.readLine()) != null) {
+                bufferTexto.write(leido1 + " --> " + leido2);
+                bufferTexto.newLine();
+                bufferTexto.write("--------------------------------");
+                bufferTexto.newLine();
+
+            }
+
+            bufferTexto.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
